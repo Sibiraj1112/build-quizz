914c3759150c92211ce4627b19034bf81d2c1633
Sibiraj1112 <162137601+Sibiraj1112@users.noreply.github.com>
2024-03-04 05:44:15 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import Settings from './pages/Settings';
+import Questions from './pages/Questions';
+import FinalScreen from './pages/FinalScreen';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Settings />
+        </Route>
+        <Route path="/questions">
+          <Questions />
+        </Route>
+        <Route path="/score">
+          <FinalScreen />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ pages/Questions.js
@@ -0,0 +1,11 @@
+
+
+const Questions = () => {
+    return (
+        <div>
+           Questions
+        </div>
+    )
+}
+
+export default Questions
